4dbe6c55e8df87b8f70861884948c1cc6a63b384
Tony Kmoch <tony.kmoch@gmail.com>
2024-07-31 13:33:06 +0200

    nastavovani anlyz

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import ttr250 from './image/ttr250.webp'
 
 function Nadpis() {
   return (
-    <div className='App'>
-      <h1 className='nadpis'>Rozdíl mezi 2 dobým a 4 dobým motorem?</h1>
-      <img src={kx250Image} alt='Na obrázku kx 250 2 takt' className='obrazek1' />
-      <p className='popis1'><b>&#8592; Kx 250 je 2 taktni motorka </b></p>
-      <img src={ttr250} alt='Na obrazku je ttr 250 4 takt' className='obrazek2' />
-      <p className='popis2'><b>Ttr250 je 4 taktni motorka &#8594;</b></p>
-    </div>
+    <Analytics>
+      <div className='App'>
+        <h1 className='nadpis'>Rozdíl mezi 2 dobým a 4 dobým motorem?</h1>
+        <img src={kx250Image} alt='Na obrázku kx 250 2 takt' className='obrazek1' />
+        <p className='popis1'><b>&#8592; Kx 250 je 2 taktni motorka </b></p>
+        <img src={ttr250} alt='Na obrazku je ttr 250 4 takt' className='obrazek2' />
+        <p className='popis2'><b>Ttr250 je 4 taktni motorka &#8594;</b></p>
+      </div>
+    </Analytics>
   );
 }
 export default Nadpis;
