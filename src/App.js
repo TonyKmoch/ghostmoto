4ad183b22aea787ec9289e8e3a58339770d97569
Tony Kmoch <tony.kmoch@gmail.com>
2024-07-31 15:07:38 +0200

    oprava05

--- src/App.js
+++ src/App.js
@@ -8,11 +8,18 @@ function Nadpis() {
   return (
     <div className='App'>
       <h1 className='nadpis'>Rozdíl mezi 2 dobým a 4 dobým motorem?</h1>
-      <img src={kx250Image} alt='Na obrázku kx 250 2 takt' className='obrazek1' />
-      <p className='popis1'><b>&#8592; Kx 250 je 2 taktni motorka </b></p>
-      <img src={ttr250} alt='Na obrazku je ttr 250 4 takt' className='obrazek2' />
-      <p className='popis2'><b>Ttr250 je 4 taktni motorka &#8594;</b></p>
+      <div className='obrazky-container'>
+        <div className='obrazek-wrapper'>
+          <img src={kx250Image} alt='Na obrázku kx 250 2 takt' className='obrazek' />
+          <p className='popis'><b>&#8592; Kx 250 je 2 taktni motorka</b></p>
+        </div>
+        <div className='obrazek-wrapper'>
+          <img src={ttr250} alt='Na obrazku je ttr 250 4 takt' className='obrazek' />
+          <p className='popis'><b>Ttr250 je 4 taktni motorka &#8594;</b></p>
+        </div>
+      </div>
     </div>
   );
 }
+
 export default Nadpis;
