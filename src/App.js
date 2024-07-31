c19f6360182a73ccb47f6ea1d2d8715a0a9f219e
Tony Kmoch <tony.kmoch@gmail.com>
2024-07-31 21:20:58 +0200

    oprava27

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ function Nadpis() {
     <div className='App'>
       <h1 className='nadpis'>Rozdíl mezi 2 dobým a 4 dobým motorem?</h1>
       <img src={kx250Image} alt='na obrazku je kx 250 2takt' className='obrazek1'></img>
-      <p className='popis1'><b>&#8592; Kx 250 je 2 taktni motorka </b></p>
+      <p className='popis popis-desktop'>
+        <b>&#8592; Kx 250 je 2 taktni motorka</b>
+      </p>
+      <p className='popis popis-mobile'>
+        <b>&#8592;Kx 250: 2 takt</b>
+      </p>
       <img src={ttr250} alt='Na obrazku je ttr 250 4 takt' className='obrazek2' />
       <p className='popis2'><b>Ttr250 je 4 taktni motorka &#8594;</b></p>
     </div>
